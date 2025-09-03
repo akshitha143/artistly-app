18edae6a56248b184b4fb0617c1d890732e11884
suryavamsi1729 <akshithaoleti@gmail.com>
2025-09-03 16:22:16 +0530

    changed robot.txt

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,7 @@
 const config = {
   siteUrl: 'https://artistly-app-puce.vercel.app',
   generateRobotsTxt: true,
+  generateIndexSitemap: true,
   changefreq: 'daily',
   priority: 0.7,
   sitemapSize: 10000,
