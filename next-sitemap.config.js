e95a10bdbf4613f256e07b7078aea67f3238f171
suryavamsi1729 <akshithaoleti@gmail.com>
2025-09-03 15:35:19 +0530

    sitemap for website

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+const config = {
+  siteUrl: 'https://artistly-app-puce.vercel.app',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 10000,
+};
+
+export default config;
